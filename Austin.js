52b81638208cdc0f08ce4ddd20c0d2276c1b5197
Austin-Command <57938480+Austin-Command@users.noreply.github.com>
2021-03-25 15:40:16 +0700

    Austin.js
    test alert .notif $/฿ Price Crypto Currency..

---
+++ Austin.js
@@ -0,0 +1,11 @@
+  // "BTC","ETH","WAN","ADA","OMG","BCH","USDT","LTC","XRP","BSV","ZIL","SNT","CVC","LINK","IOST","ZRX","KNC","RDN","ABT","MANA","INF","CTXC","XLM","SIX","JFIN","EVX","BNB","POW","DOGE","DAI","USDC","BAT","BAND","KSM","DOT","NEAR","SCRT","GLM" 
+  // มีัทั้งหมด 71 ตัว เลือกเอาไปใส่ในตัวแปร listSymbol ได้เลยน้าา
+
+function main() {
+
+  var listSymbol = ["BTC","ETH","WAN","ADA","OMG","BCH","USDT"];
+  var token = "นำ token มาใส่ตรงนี้";
+
+  funtionALL(listSymbol,token)
+  Logger.log("สำเร็จแล้วน้า")
+}
